7d1dc627c04c4ae89f9b6688423e7744afff5cba
Stéphane Klein <contact@stephane-klein.info>
2022-06-01 08:23:14 +0200

    Add hooks.ts boilerplate

---
+++ src/hooks.ts
@@ -0,0 +1,39 @@
+import {
+	ConfigurePassportOAuth2,
+	OAuthHandleInput,
+	OAuthCreateCookie,
+	DefaultCookieName
+} from 'sveltekit-passport-oauth2';
+import GitlabStrategy from "passport-gitlab2";
+
+ConfigurePassportOAuth2([
+	{
+		strategy: new GitlabStrategy(
+			{
+				callbackURL: 'http://127.0.0.1:3000/auth/gitlab/callback.json',
+				clientID: process.env.GITLAB_CLIENT_ID,
+				clientSecret: process.env.GITLAB_CLIENT_SECRET,
+				// passReqToCallback: true,
+                baseURL: process.env.GITLAB_BASEURL
+			},
+			OAuthCreateCookie(/*cookieName = DefaultCookieName, cookieSettings?: CookieSettings*/)
+		)
+	}
+]);
+export const handle = sequence(addUserToRequest);
+
+//add the user info to request (you can access this info in and endpoint using `request.locals`)
+async function addUserToRequest({ request, resolve }: OAuthHandleInput) {
+	const cookies = cookie.parse(request.headers.cookie || '');
+	const cookieId = cookies[DefaultCookieName];
+	if (cookieId) {
+		request.locals = Database[cookieId]; //change this to retrieve from database
+	}
+	const response = await resolve(request);
+	return response;
+}
+
+//this will expose user info in session and can be retrieved in the front end using $session
+export function getSession(request: ServerRequest<Locals>): Locals {
+	return request.locals;
+}
