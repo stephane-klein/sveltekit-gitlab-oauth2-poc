295d5733bd47f90ed34fc9583e38d2289addab37
Stéphane Klein <contact@stephane-klein.info>
2022-06-03 12:37:13 +0200

    Coding style

--- src/server.js
+++ src/server.js
@@ -5,22 +5,23 @@ import GitLabStrategy from 'passport-gitlab2';
 const app = express()
 const port = 3000
 
-passport.use(new GitLabStrategy({
-    clientID: process.env.GITLAB_CLIENT_ID,
-    clientSecret: process.env.GITLAB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/gitlab/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({gitlabId: profile.id}, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+passport.use(
+    new GitLabStrategy(
+        {
+            clientID: process.env.GITLAB_CLIENT_ID,
+            clientSecret: process.env.GITLAB_CLIENT_SECRET,
+            callbackURL: "http://localhost:3000/auth/gitlab/callback"
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            return cb(err, user);
+        }
+    )
+);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
