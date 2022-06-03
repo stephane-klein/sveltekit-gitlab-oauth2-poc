243b3fe5cdb47cb324cc5ac74c48d211802bb7e6
Stéphane Klein <contact@stephane-klein.info>
2022-06-03 19:27:24 +0200

    Migrate fro passport-gitlab2 to passport-oauth2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
+import url from 'node:url';
 import express from 'express';
 import passport from 'passport';
-import GitLabStrategy from 'passport-gitlab2';
+import OAuth2Strategy from 'passport-oauth2';
+import { InternalOAuthError } from 'passport-oauth2';
 import session from 'express-session';
 
 const app = express();
@@ -24,30 +26,62 @@ passport.deserializeUser(function(user, cb) {
     });
 });
 
-passport.use(
-    new GitLabStrategy(
-        {
-            clientID: process.env.GITLAB_CLIENT_ID,
-            clientSecret: process.env.GITLAB_CLIENT_SECRET,
-            callbackURL: 'http://127.0.0.1:3000/auth/gitlab/callback',
-            baseURL: process.env.GITLAB_BASEURL
-        },
-        function(accessToken, refreshToken, profile, cb) {
-            return cb(
-                null,
-                {
-                    id: profile.id,
-                    username: profile.username,
-                    accessToken: accessToken
-                }
-            );
-        }
-    )
+const gitlabOAuth2Strategy = new OAuth2Strategy(
+    {
+        // configuration inspired from https://github.com/fh1ch/passport-gitlab2/blob/4238b67438c1f1a7050908556ac010bc319b734a/lib/strategy.js
+        clientID: process.env.GITLAB_CLIENT_ID,
+        clientSecret: process.env.GITLAB_CLIENT_SECRET,
+        authorizationURL: url.resolve(process.env.GITLAB_BASEURL, 'oauth/authorize'),
+        tokenURL: url.resolve(process.env.GITLAB_BASEURL, 'oauth/token'),
+        scope: "api email profile",
+        scopeSeparator: " ",
+        callbackURL: 'http://127.0.0.1:3000/auth/gitlab/callback'
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        return cb(
+            null,
+            {
+                id: profile.id,
+                username: profile.username,
+                accessToken: accessToken
+            }
+        );
+    }
 );
 
+gitlabOAuth2Strategy.userProfile = function (accesstoken, done) {
+    this._oauth2.get(
+        url.resolve(process.env.GITLAB_BASEURL, 'api/v4/user'),
+        accesstoken,
+        (err, body) => {
+            let json;
+            if (err) {
+                return done(new InternalOAuthError('Failed to fetch user profile', err));
+            }
+
+            try {
+                json = JSON.parse(body);
+            } catch (ex) {
+                return done(new Error('Failed to parse user profile'));
+            }
+
+            const profile = {
+                id: String(json.id),
+                username: json.username,
+                displayName: json.name,
+                emails: [{value: json.email}],
+                avatarUrl: json.avatar_url,
+                profileUrl: json.web_url
+            };
+
+            done(null, profile);
+        }
+    );
+};
+
+passport.use(gitlabOAuth2Strategy);
+
 app.get('/', (req, res) => {
-    console.log('req.session');
-    console.log(req.session);
     if (req.session?.passport?.user?.id) {
         res.send(`<ul>
             <li>id: ${req.session.passport.user.id}</li>
@@ -62,16 +96,13 @@ app.get('/', (req, res) => {
 app.get(
     '/auth/gitlab',
     passport.authenticate(
-        'gitlab',
-        {
-            scope: ['api email profile'] // See https://github.com/fh1ch/passport-gitlab2/pull/17
-        }
+        'oauth2'
     )
 );
 
 app.get(
     '/auth/gitlab/callback',
-    passport.authenticate('gitlab', {
+    passport.authenticate('oauth2', {
         failureRedirect: '/login'
     }),
     function(req, res) {
