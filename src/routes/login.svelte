4906450f6df14ec5ff8898fcdd0ed012977d7658
Stéphane Klein <contact@stephane-klein.info>
2022-06-08 07:32:15 +0200

    Add SSR page with hydrate disabled

---
+++ src/routes/login.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+    export const hydrate = false;
+    export async function load({ params, fetch, session, stuff }) {
+        console.log('loading');
+        return {
+            status: 200
+        }
+    };
+</script>
